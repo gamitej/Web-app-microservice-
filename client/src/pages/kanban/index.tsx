5a86aab1eca4ad294f9564ce86bfdadd6411d1db
gamitej <gamitejpratapsingh998@gmail.com>
2025-02-15 18:27:01 +0530

    chore : basic kanban design done with dummy data

--- client/src/pages/kanban/index.tsx
+++ client/src/pages/kanban/index.tsx
@@ -39,21 +39,29 @@ const KanbanPage = () => {
    * TSX
    */
   return (
-    <div className="w-full p-6 flex flex-col gap-6 justify-center items-center">
-      <h2 className="text-xl font-bold text-gray-600 text-center uppercase">
-        Kanban Board
-      </h2>
+    <div className="w-full md:w-1/2 m-auto p-6 flex flex-col gap-6 justify-center items-center mt-10">
+      <div className="flex justify-center items-center gap-4">
+        <h2 className="text-xl font-bold text-gray-600 uppercase">
+          Kanban Board
+        </h2>
+        <button className="rounded-md px-4 py-1 shadow bg-gray-600 text-white hover:bg-gray-500">
+          Add
+        </button>
+      </div>
       <div className="w-full grid grid-cols-3 gap-4">
         {kanbanStatusColumns.map(({ label, value }: ColumnsType) => (
-          <div key={value} className="col-span-1">
+          <div key={value} className="col-span-1 flex flex-col gap-4">
             <h3 className="font-bold uppercase text-blue-400 bg-gray-100 text-center py-2">
               {label}
             </h3>
             {tasks
               .filter((task) => task.status === value)
               .map(({ id, title, description }) => (
-                <div key={id} className="border rounded-md shadow p-2">
-                  <h5>{title}</h5>
+                <div
+                  key={id}
+                  className="border rounded-md shadow p-2 cursor-pointer hover:shadow-md hover:shadow-yellow-300"
+                >
+                  <h5 className="text-gray-800">{title}</h5>
                   <span className="text-gray-400">{description}</span>
                 </div>
               ))}
