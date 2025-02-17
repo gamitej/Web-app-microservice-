1a4f47c60968b10a7e9cbca4c9389aa5421f7a2b
gamitej <gamitejpratapsingh998@gmail.com>
2025-02-17 19:48:09 +0530

    chore : table done

--- client/src/components/table/TableFooter.tsx
+++ client/src/components/table/TableFooter.tsx
@@ -1,10 +1,46 @@
-const TableFooter = ({ noOfRows = 0 }: { noOfRows: number }) => {
-  /**
-   * TSX
-   */
+import { IoIosArrowForward as RightArrowIcon } from "react-icons/io";
+
+interface TableFooterProps {
+  totalRows: number;
+  totalPages: number;
+  pagination: number;
+  handlePagination: (val: -1 | 1) => void;
+}
+
+const TableFooter = ({
+  totalRows = 0,
+  pagination,
+  totalPages = 0,
+  handlePagination,
+}: TableFooterProps) => {
   return (
-    <div className="flex justify-between items-center py-3 px-4 text-blue-500">
-      Rows - {noOfRows}
+    <div className="flex justify-between items-center py-3 px-4 text-blue-500 font-[550]">
+      <div>Rows - {totalRows}</div>
+      <div className="flex justify-center items-center gap-2">
+        <span
+          aria-disabled={pagination === 0}
+          onClick={() => handlePagination(-1)}
+          className={`cursor-pointer hover:bg-gray-200 rounded-full p-2 ${
+            pagination === 0 ? "text-gray-400 cursor-not-allowed" : ""
+          }`}
+        >
+          <RightArrowIcon className="-rotate-180 text-2xl mr-1" />
+        </span>
+        <span>
+          {pagination + 1} / {totalPages}
+        </span>
+        <span
+          aria-disabled={pagination === totalPages - 1}
+          onClick={() => handlePagination(1)}
+          className={`cursor-pointer hover:bg-gray-200 rounded-full p-2 ${
+            pagination === totalPages - 1
+              ? "text-gray-400 cursor-not-allowed"
+              : ""
+          }`}
+        >
+          <RightArrowIcon className="text-2xl ml-1" />
+        </span>
+      </div>
     </div>
   );
 };
--- client/src/components/table/TableToolbar.tsx
+++ client/src/components/table/TableToolbar.tsx
@@ -3,7 +3,7 @@ const TableToolbar = () => {
    * TSX
    */
   return (
-    <div className="px-4 py-3 flex justify-between items-center">
+    <div className="px-4 py-4 flex justify-between items-center">
       <div>
         <h3 className="text-blue-500 font-[550] uppercase">Table Name</h3>
       </div>
